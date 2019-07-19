4e9ee4ce91de8ce28fe9485d1b5c1ab06621484a
Guilherme Moura <guilhermemouraa5@gmail.com>
2019-07-19 17:38:27 -0300

    Adicionar Atividades

---
+++ script.js
@@ -0,0 +1,96 @@
+// let idade = Number(prompt("Digite sua idade"));
+
+// let aposentado = prompt("Você é Aposentado?")
+
+// let estudante = prompt("Você é estudante?")
+    
+
+// if(idade >= 60 || aposentado == "sim"){
+//     alert("entrada gratis");
+// }
+
+
+// else if(idade >0 && idade <=12){
+//     alert("Sua entrada é R$10,00 ");
+// }
+
+// else if(idade >12 && idade <=17){
+//     alert("Sua entrada é R$15,00 ");
+// }
+
+// else if(idade >17 && idade <=59){
+//     if (estudante == "sim"){
+//         alert("Meia Entrada, R$17,50");
+//     } else {
+//         alert("Sua entrada é R$35,00 ");
+//     }
+// }
+// else {
+//     alert("Tente Novamente");
+// }
+
+
+
+
+
+
+
+
+
+// let semana=Number(prompt("Digite o dia do mês"));
+// if(semana == 1){
+//     alert("È domingo")
+// }
+
+// else if(semana == 2){
+//     alert("é segunda-feira")
+// }
+
+// else if(semana == 3){
+//     alert("È terça-feira")
+// }
+
+// else if(semana == 4){
+//     alert("é quarta-feira")
+// }
+
+// else if(semana == 5){
+//     alert("é quinta-feira")
+// }
+
+// else if(semana == 6){
+//     alert("é sexta-feira")
+// }
+
+// else if(semana == 7){
+//     alert("é sabado" )
+// }
+
+// else {
+//     alert("Erro, Escreva um numero de 1 a 7")
+// }
+
+
+// let idade = Number(prompt("Digite sua idade"));
+
+
+// if(idade >=25 && idade <=60){
+//     let salario = Number(prompt("Digite seu salario"));
+//         if(salario >=2000){
+//             let emprestimo = Number(prompt("Digite o valor do emprestimo desejado"));
+//                 if(emprestimo <10*salario){
+//                     alert("Emprestimo Aprovado")
+//                 }else{
+//                     alert("Emprestimo recusado,Valor do emprestimo inregular")
+//                 }
+//         }else{
+//             alert("Emprestimo recusado, salario inregular")
+//         }
+
+// }else{
+//     alert("Emprestimo recusado, idade inregular")
+// }
+
+
+
+
